dea2c43b4e2a44aaa4ebc186a30a1b0d87644d45
Leliksey <captainfroody@gmail.com>
2021-02-17 12:52:10 +0200

    all

--- assets/js/components/modal.js
+++ assets/js/components/modal.js
@@ -1,1 +1,42 @@
-console.log('modal');
+function hideMenuDropDown() { 
+    var menuDropdownShowArray = document.querySelectorAll(".menu__dropdown_show");
+    menuDropdownShowArray.forEach(function(menuDropdownShow) {
+         menuDropdownShow.classList.remove("menu__dropdown_show");
+    })
+}
+
+document.getElementById("burger").addEventListener('click', () => {
+    document.body.classList.toggle('body_menu-open');
+    hideMenuDropDown();
+});
+
+document.body.addEventListener('click', e => {
+    if( document.body.classList.contains('body_menu-open') ) {  
+        if( e.target.classList.contains('burger') === false &&
+            e.target.classList.contains('menu') === false &&
+            e.target.classList.contains('menu__item') === false &&
+            e.target.classList.contains('menu__link')  === false 
+            ) {
+            document.body.classList.remove('body_menu-open');
+            hideMenuDropDown();
+        }
+    }
+});
+
+var menuLinkArray =  document.querySelectorAll(".menu__link");
+menuLinkArray.forEach(function(menuLink) {
+    menuLink.addEventListener('click', e => {
+        e.preventDefault();
+        var menuItem = menuLink.parentNode;
+        var menuDropdown = menuItem.querySelector(".menu__dropdown");
+        menuDropdown.classList.toggle('menu__dropdown_show')
+
+        var menuDropdownShowArray = document.querySelectorAll(".menu__dropdown_show");
+        menuDropdownShowArray.forEach(function(menuDropdownShow) {
+            if (menuDropdownShow != menuDropdown) {
+                menuDropdownShow.classList.remove("menu__dropdown_show");
+            }
+        })
+    });
+})
+
